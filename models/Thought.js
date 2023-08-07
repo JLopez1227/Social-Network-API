56ee81a925d57ee6615f9be0910ab27a8f584482
JLopez1227 <josuenmilopez27@gmail.com>
2023-08-07 16:03:53 -0500

    Worked on models got them set up how instructions specify.

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: () => new Date(),
+      get: (date)=> new Date(date).toLocaleDateString()
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
